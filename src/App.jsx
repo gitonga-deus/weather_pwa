d79e192cb87c3f87adf63db245a3a3c520a0717e
gitonga <gitonga.deus@gmail.com>
2022-11-01 18:16:44 +0300

    Done with the front-end section for displaying weather data

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import { fetchWeather } from "./api/fetchWeather";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	const [query, setQuery] = useState("");
+	const [weather, setWeather] = useState({});
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	const search = async (e) => {
+		if (e.key === "Enter") {
+			const data = await fetchWeather(query);
+
+			setWeather(data);
+			setQuery("");
+		}
+	};
+
+	return (
+		<div className="gradient-bg-welcome justify-center flex-col outline-none items-center flex">
+			<input
+				type="text"
+				className="p-2 w-72 h-[40px] rounded-full text-center"
+				placeholder="Search..."
+				value={query}
+				onChange={(e) => setQuery(e.target.value)}
+				onKeyPress={search}
+			/>
+			{weather.main && (
+				<div className="city flex items-center justify-center flex-col mt-5 text-white">
+					<h2 className="city-name items-center justify-center text-white">
+						<span>{weather.name}</span>
+						<sup>{weather.sys.country}</sup>
+					</h2>
+					<div className="text-3xl font-bold mt-[10px] text-white text-center">
+						{Math.round(weather.main.temp)}
+						<sup>&deg;C</sup>
+					</div>
+					<div className="flex flex-col items-center">
+						<img
+							className="mt-[10px] w-[100px] h-[100px]"
+							src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+							alt={weather.weather[0].description}
+						/>
+						<p>{weather.weather[0].description}</p>
+					</div>
+				</div>
+			)}
+		</div>
+	)
 }
 
-export default App
+export default App;
